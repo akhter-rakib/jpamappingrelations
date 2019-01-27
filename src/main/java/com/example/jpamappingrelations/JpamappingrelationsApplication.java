4d9ea8f49e8dde9dae77a54aa8806afa3dcf62a9
rakibccj <rakibccj@gmail.com>
2019-01-27 23:13:12 +0600

    project initiate

---
+++ src/main/java/com/example/jpamappingrelations/JpamappingrelationsApplication.java
@@ -0,0 +1,14 @@
+package com.example.jpamappingrelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpamappingrelationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpamappingrelationsApplication.class, args);
+    }
+
+}
+
